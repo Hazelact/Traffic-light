fdb7614e56681eb425b8f6fa0d645f8295550eba
Hazelact <hazelskyeye1@gmail.com>
2026-05-10 15:29:58 +0600

    Add files via upload

---
+++ traffic-light.py
@@ -0,0 +1,12 @@
+light=input("light colour:")
+
+if(light=="red"):
+    print("stop")
+    print("you can't move")
+    print("it's dangerous")
+elif(light=="green"):
+    print("go.Don't stop,or u will cause the trouble for other cars which is next behind u")
+elif(light=="yellow"):
+    print("look and then go to ur way")
+else:
+    print("light is broken")
